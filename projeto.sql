ce3ce7556a1cf01e8088a18d0099ddb94119c595
Nicolas C L Barletta <58794347+NicolasBarletta@users.noreply.github.com>
2023-11-14 14:14:56 -0300

    Adicionado arquivos SQL
    O arquivo projeto.sql possui os itens exportados diretamente do MySQL.
    O arquivo codigossql possui qual é o código necessário para criar o mesmo banco de dados.

---
+++ projeto.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 14-Nov-2023 às 17:09
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `projeto`
+--
+
+DROP TABLE IF EXISTS `projeto`;
+CREATE TABLE IF NOT EXISTS `projeto` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cpf` bigint NOT NULL,
+  `creci` int NOT NULL,
+  `nome` varchar(220) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `projeto`
+--
+
+INSERT INTO `projeto` (`id`, `cpf`, `creci`, `nome`) VALUES
+(1, 11324944439, 22, 'Nicolas Barletta'),
+(4, 43713798850, 123456, 'joao');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
